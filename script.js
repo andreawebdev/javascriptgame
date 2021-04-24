5f062a42f33db3b074c8e9d341bdee1d334c21b3
andrea capizzi <andreaitalia2020@libero.it>
2021-04-24 15:13:47 +0200

    end

--- script.js
+++ script.js
@@ -1,34 +1,26 @@
-'use strict';
+"use strict";
 let myAge = Math.trunc(Math.random() * 44) + 1;
 // let myAge = 10;
 
-
 let score = 5;
 
+document.querySelector(".myBotton").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
 
-
-
-document.querySelector('.myBotton').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
-  
-
-  
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔️ Please insert a number between 25 and 55!';
-
-   } 
-  else if (guess === myAge) {
+    document.querySelector(".message").textContent =
+      "⛔️ Please insert a number between 25 and 55!";
+  } else if (guess === myAge) {
     document.querySelector(".message").textContent =
       "🎉 Correct Number!You won!";
 
-
-    document.querySelector("body").style.backgroundColor="rgb(95,24,23)";
-    document.querySelector("audio").play()
-  
- 
- 
+    document.querySelector("body").style.backgroundColor = "rgb(95,24,23)";
+    document.querySelector("audio").play();
   } else if (guess < 0) {
     document.querySelector(".message").textContent = "Game Over!";
+document.querySelector("body").style.backgroundColor = "rgb(150,24,23)";
+    document.querySelector("#gameover").play();
+    
   } else if (guess > 55) {
     document.querySelector(".message").textContent =
       "⛔️ Number too High! Please insert a number between 25 and 45!";
@@ -42,14 +34,9 @@ document.querySelector('.myBotton').addEventListener('click', function () {
       document.querySelector(".score").textContent = score;
     } else {
       document.querySelector(".message").textContent = "Game Over!";
-       document.querySelector("#gameover").play();
-        document.querySelector("body").style.backgroundColor = "rgb(150,24,23)";
+      document.querySelector("body").style.backgroundColor = "rgb(150,24,23)";
+      document.querySelector("#gameover").play();
+      
     }
   }
-
-
 });
-
-
-
-
